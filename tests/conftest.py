b674ece15d7cf6a2da75474a1e354c06f9971590
Sohaib Farooqi <sohaibfarooqi@hotmail.com>
2019-01-19 20:31:34 +0800

    test cases for models, schema, consumer

---
+++ tests/conftest.py
@@ -0,0 +1,80 @@
+import pytest, os
+from sqlalchemy import create_engine
+from sqlalchemy_utils import database_exists, create_database, drop_database
+from sqlalchemy.orm import sessionmaker
+from alembic.command import upgrade as alembic_upgrade
+from alembic.config import Config as AlembicConfig
+
+uri = os.environ.get('DATABASE_URI')
+
+@pytest.fixture
+def db():
+  """
+  Fixture to create test database, apply migrations
+  and finally delete it once the test cases are executed.
+  """
+  if not database_exists(uri):
+    create_database(uri)
+  alembic_config = AlembicConfig('alembic.ini')
+  alembic_config.set_main_option('sqlalchemy.url', uri)
+  alembic_upgrade(alembic_config, 'head')
+  yield "success"
+  drop_database(uri)
+
+@pytest.fixture
+def db_session(db):
+  """
+  Fixture to provide SQLAlchemy session for querying.
+  """
+  engine = create_engine(uri)
+  Session = sessionmaker(bind=engine)
+  session = Session()
+  yield session
+
+@pytest.fixture
+def valid_data():
+  """
+  Fixture to load valid data according to specified schema.
+  """
+  d1 = {
+    'name': 'Milla Nova',
+    'timestamp': 1547865754.330828,
+    'email': 'catos.sadf.df@google.com.hk',
+    'parent_task_id': '92487b0e-211f-4b1a-bf11-55429760ba68'
+  }
+  yield d1
+
+@pytest.fixture
+def duplicate_email():
+  """
+  This Fixture contains duplicate emails. It is
+  used to test unique constraint violations.
+  """
+  d2 = [
+    {
+      'name': 'Milla Nova',
+      'timestamp': 1547865754.330828,
+      'email': 'catos.sadf.df@google.com.hk',
+      'parent_task_id': '92487b0e-211f-4b1a-bf11-55429760ba68'
+    },
+    {
+      'name': 'Milla Nova',
+      'timestamp': 1547865754.330828,
+      'email': 'catos.sadf.df@google.com.hk',
+      'parent_task_id': '92487b0e-211f-4b1a-bf11-55429760ba68'
+    }
+  ]
+  yield d2
+
+@pytest.fixture
+def invalid_data():
+  """
+  Fixture with invalid email address format
+  """
+  d3 = {
+    'name': 'Milla Nova',
+    'timestamp': 1547865754.330828,
+    'email': 'catos.sadf',
+    'parent_task_id': '92487b0e-211f-4b1a-bf11-55429760ba68'
+  }
+  yield d3
---
+++ tests/test_schema.py
@@ -0,0 +1,20 @@
+import pytest
+from marshmallow.exceptions import ValidationError
+from consumer.schema import UserSchema
+
+def test_schema_valid_data(valid_data):
+  """
+  Test schema with valid data should not
+  raise any error.
+  """
+  schema = UserSchema()
+  assert schema.validate(valid_data) == {}
+
+def test_schema_invalid_data(invalid_data):
+  """
+  Test with invalid data schema should raise
+  marshmallow ValidationError.
+  """
+  schema = UserSchema()
+  with pytest.raises(ValidationError):
+    assert schema.load(invalid_data)
