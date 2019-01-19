1a7a680e1fe1da918a0530342c60001d1078e46c
Sohaib Farooqi <sohaibfarooqi@hotmail.com>
2019-01-19 20:29:15 +0800

    added db creation to migration script

--- alembic/env.py
+++ alembic/env.py
@@ -6,6 +6,7 @@ from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
 from alembic import context
+from sqlalchemy_utils import database_exists, create_database
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -27,6 +28,9 @@ target_metadata = None
 # ... etc.
 
 config.set_main_option('sqlalchemy.url', os.environ.get('DATABASE_URI'))
+if not database_exists(config.get_main_option("sqlalchemy.url")):
+    create_database(config.get_main_option("sqlalchemy.url"))
+
 def run_migrations_offline():
     """Run migrations in 'offline' mode.
 
